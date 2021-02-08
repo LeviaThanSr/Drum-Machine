c03f5e70c8704f9dd6c22005ef33bd81ebbd3187
LeviaThanSr <omarsarour@gmail.com>
2021-02-08 15:39:36 +0100

    Starting The Project

---
+++ src/drumMachine.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DrumMachine extends Component {
+  render() {
+    return (
+      <div className="drum-machine">
+        <h1>Drum Machine</h1>
+      </div>
+    );
+  }
+}
+
+export default DrumMachine;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import DrumMachine from "./drumMachine";
+import "./style.css";
+
+ReactDom.render(<DrumMachine />, document.getElementById("root"));
