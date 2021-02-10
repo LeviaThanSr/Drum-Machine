a180119108886cf65c86ca049af57f6b1d50cf3e
LeviaThanSr <omarsarour@gmail.com>
2021-02-10 20:58:18 +0100

    Added audioNameArr

---
+++ src/audioNameArr.js
@@ -0,0 +1,21 @@
+export const AudioNameArr = {
+  Q: "Heater-1",
+
+  W: "Heater-2",
+
+  E: "Heater-3",
+
+  A: "Heater-4",
+
+  S: "Clap",
+
+  D: "Open-HH",
+
+  Z: "Kick-n'-Hat",
+
+  X: "Kick",
+
+  C: "Closed-HH",
+};
+
+export default AudioNameArr;
