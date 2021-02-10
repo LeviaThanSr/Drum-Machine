fe0db6a59596c71369a6bb691722647b2c0e94c1
LeviaThanSr <omarsarour@gmail.com>
2021-02-10 02:17:31 +0100

    Update drumMachine

--- src/drumMachine.js
+++ src/drumMachine.js
@@ -1,28 +1,18 @@
-import React, { Component } from "react";
-import DrumBody from "./drumBody";
+import React from "react";
+import audioArr from "./audioArr";
+import DrumPad from "./drumPad";
 
-class DrumMachine extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      buttons: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
-    };
-  }
-  render() {
-    const { buttons } = this.state;
-    return (
-      <div className="container" id="drum-machine">
-        <div
-          id="display"
-          className="drum-pad-body rounded border border-2 border-primary shadow"
-        >
-          {buttons.map((key, id) => (
-            <DrumBody innerText={key} key={"drum-" + id} />
-          ))}
-        </div>
+const DrumMachine = () => (
+  <div id="drum-machine" className="container-fluid ">
+    <div id="display" className="card bg-info">
+      <h3 className="text-center">hi</h3>
+      <div className="container drum-pad-body rounded border border-2 border-primary shadow">
+        {audioArr.map((i, id) => (
+          <DrumPad innerText={i.key} audio={i.url} key={id} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default DrumMachine;
